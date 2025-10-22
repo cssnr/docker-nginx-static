e492fbc1c06e75758090669bd495aa87791cf602
SmAsHeD <6071159+smashedr@users.noreply.github.com>
2025-10-21 20:28:12 -0700

    Add try_files and 404 (#2)

--- src/10-setup.sh
+++ src/10-setup.sh
@@ -14,10 +14,14 @@ echo "NGINX_INDEX: ${NGINX_INDEX:=$_NGINX_INDEX}"
 sed "s/NGINX_INDEX/${NGINX_INDEX:=$_NGINX_INDEX}/g" \
     -i /etc/nginx/nginx.conf
 
+rm -f /etc/nginx/conf.d/http.gzip.conf
+rm -f /etc/nginx/conf.d/location.auth.conf
+rm -f /etc/nginx/auth.users
+
 : "${GZIP_TYPES:=*}"
 echo "GZIP_TYPES: ${GZIP_TYPES}"
 if [ "${GZIP_TYPES}" != "off" ];then
-cat <<EOF > /etc/nginx/conf.d/http.gzip.conf
+cat <<EOF >> /etc/nginx/conf.d/http.gzip.conf
 gzip            on;
 gzip_proxied    any;
 gzip_min_length ${GZIP_LENGTH:-1000};
@@ -25,10 +29,16 @@ gzip_types      ${GZIP_TYPES};
 EOF
 fi
 
+: "${ERROR_PAGE:=/404.html}"
+if [ "${ERROR_PAGE}" != "off" ];then
+    echo "ERROR_PAGE: ${ERROR_PAGE}"
+    echo "error_page 404 ${ERROR_PAGE};" >> /etc/nginx/conf.d/location.auth.conf
+fi
+
 if [ -n "${BASIC_AUTH}" ];then
 echo "BASIC_AUTH: ${BASIC_AUTH}"
 printf '%s' "${BASIC_AUTH}" > /etc/nginx/auth.users
-cat <<EOF > /etc/nginx/conf.d/location.auth.conf
+cat <<EOF >> /etc/nginx/conf.d/location.auth.conf
 auth_basic            "${BASIC_REALM:-Unauthorized}";
 auth_basic_user_file  /etc/nginx/auth.users;
 EOF
